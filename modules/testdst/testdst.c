1582546ece960615b16774a9208a84208721248c
videet <videetssinghai@gmail.com>
2018-05-20 01:42:26 +0530

    added queue method

--- modules/testdst/testdst.c
+++ modules/testdst/testdst.c
@@ -20,6 +20,9 @@ typedef struct _TestDstDriver
   time_t suspend_until;
 } TestDstDriver;
 
+G_LOCK_DEFINE_STATIC(testdst_lock);
+
+
 LogTemplateOptions *
 testdst_dd_get_template_options(LogDriver *s)
 {
@@ -37,6 +40,22 @@ testdst_dd_set_template(LogDriver *s, LogTemplate *template)
   self->template = template;
 }
 
+static gboolean
+_is_output_suspended(TestDstDriver *self, time_t now)
+{
+  if (self->suspend_until && self->suspend_until > now)
+    return TRUE;
+  return FALSE;
+}
+
+static void
+_suspend_output(TestDstDriver *self, time_t now)
+{
+  GlobalConfig *cfg = log_pipe_get_config(&self->super.super.super);
+
+  self->suspend_until = now + cfg->time_reopen;
+}
+
 static EVTTAG *
 _evt_tag_message(const GString *msg)
 {
@@ -46,6 +65,13 @@ _evt_tag_message(const GString *msg)
                         (int) MIN(max_len, msg->len), msg->str,
                         msg->len > max_len ? "..." : "");
 }
+
+static void
+_format_message(TestDstDriver *self, LogMessage *msg, GString *formatted_message)
+{
+  log_template_format(self->template, msg, &self->template_options, LTZ_LOCAL, 0, NULL, formatted_message);
+}
+
 
 static gboolean
 _write_message(TestDstDriver *self, const GString *msg)
@@ -97,6 +123,32 @@ exit:
   return success;
 }
 
+static void
+testdst_dd_queue(LogPipe *s, LogMessage *msg, const LogPathOptions *path_options)
+{
+  TestDstDriver *self = (TestDstDriver *) s;
+  GString *formatted_message = scratch_buffers_alloc();
+  gboolean success;
+  time_t now = msg->timestamps[LM_TS_RECVD].tv_sec;
+
+  if (_is_output_suspended(self, now))
+    goto finish;
+
+  _format_message(self, msg, formatted_message);
+
+  G_LOCK(testdst_lock);
+  success = _write_message(self, formatted_message);
+  G_UNLOCK(testdst_lock);
+
+  if (!success)
+    _suspend_output(self, now);
+
+finish:
+  log_dest_driver_queue_method(s, msg, path_options);
+}
+
+
+
 static gboolean
 testdst_dd_init(LogPipe *s)
 {
@@ -127,6 +179,7 @@ testdst_dd_new(gchar *testfile_name, GlobalConfig *cfg)
   log_template_options_defaults(&self->template_options);
   self->super.super.super.init = testdst_dd_init;
   self->super.super.super.free_fn = testdst_dd_free;
+  self->super.super.super.queue = testdst_dd_queue;
   self->testfile_name = g_strdup(testfile_name);
   return &self->super.super;
 }
